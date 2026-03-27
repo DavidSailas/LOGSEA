f0dd6bfcdd2a9c4cdd567c796d425307b8e29fcb
DavidSailas <chebry28@gmail.com>
2026-03-27 14:58:27 +0800

    feat: add header and hero section UI

--- front-end/src/Components/Hero.jsx
+++ front-end/src/Components/Hero.jsx
@@ -1,21 +1,19 @@
 import React from "react";
-import heroImg from "../assets/hero.png";
 
 function Hero() {
 	return (
 		<section className="hero" id="home">
 			<div className="hero-text">
-				<h1>Empowering Children with Special Needs to Learn and Grow</h1>
-				<p>
-					Dedicated to nurturing and educating children with special needs.
-				</p>
-				<a href="#enroll" className="cta-btn">
-					Learn More
-				</a>
-			</div>
+				<h1>
+					Empowering Children with <br />
+					Special Needs to Learn and Grow
+				</h1>
 
-			<div className="hero-image">
-				<img src={heroImg} alt="Academy" style={{ maxWidth: 420 }} />
+				<div className="hero-actions">
+					<a href="#enroll" className="cta-btn">
+					Learn More
+					</a>
+				</div>
 			</div>
 		</section>
 	);
